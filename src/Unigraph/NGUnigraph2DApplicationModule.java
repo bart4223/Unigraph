958eb02ae218b0d4ffae08fb90c7391370910a6d
Nils Grimmer <nilsgrimmer@web.de>
2015-09-16 18:42:45 +0200

    Rename

---
+++ src/Unigraph/NGUnigraph2DApplicationModule.java
@@ -0,0 +1,12 @@
+package Unigraph;
+
+import Uniwork.Appl.NGVisualApplicationModule;
+import Uniwork.Base.NGComponent;
+
+public class NGUnigraph2DApplicationModule extends NGVisualApplicationModule {
+
+    public NGUnigraph2DApplicationModule(NGComponent aOwner, String aName) {
+        super(aOwner, aName);
+    }
+
+}

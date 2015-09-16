1eb4720727a68730f6ca30eb5dd45c0cc5637dc8
Nils Grimmer <nilsgrimmer@web.de>
2015-09-16 21:03:19 +0200

    Base diagram object

---
+++ src/Unigraph/Objects/NGUGCustomDiagramObject.java
@@ -0,0 +1,11 @@
+package Unigraph.Objects;
+
+import Uniwork.Base.NGObject;
+
+public abstract class NGUGCustomDiagramObject extends NGObject {
+
+    public NGUGCustomDiagramObject() {
+        super();
+    }
+
+}

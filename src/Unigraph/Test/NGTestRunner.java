1f5f986037c006cfc682061cf474e1e0ba70e618
Nils Grimmer <nilsgrimmer@web.de>
2015-11-02 18:54:20 +0100

    Rework Test Runner

--- src/Unigraph/Test/NGTestRunner.java
+++ src/Unigraph/Test/NGTestRunner.java
@@ -1,27 +1,18 @@
 package Unigraph.Test;
 
+import Uniwork.Test.NGCustomTestRunner;
 import Unigraph.Test.Units.NGAllTestUnits;
-import Uniwork.Misc.NGConsoleLogEventListener;
-import Uniwork.Misc.NGLogManager;
-import Uniwork.Test.NGTeamcityLogEventListener;
-import org.junit.runner.JUnitCore;
-import org.junit.runner.Result;
-import org.junit.runner.notification.Failure;
 
-public class NGTestRunner {
+public class NGTestRunner extends NGCustomTestRunner{
 
-    protected static NGConsoleLogEventListener FLogListener;
+    public NGTestRunner(Class aTestClass) {
+        super(aTestClass);
+    }
 
     public static void main(String[] args) {
-        FLogListener = new NGTeamcityLogEventListener();
-        Uniwork.Test.NGTestRunner.Logger = new NGLogManager();
-        Uniwork.Test.NGTestRunner.Logger.addEventListener(FLogListener);
-        Uniwork.Test.NGTestRunner.Logger.writeLog("message text='Test run started...'");
-        Result result = JUnitCore.runClasses(NGAllTestUnits.class);
-        for (Failure failure : result.getFailures()) {
-            Uniwork.Test.NGTestRunner.Logger.writeLog(String.format("message text='Error' errorDetails='%s'", failure.toString()));
-        }
-        Uniwork.Test.NGTestRunner.Logger.writeLog("message text='Test run finished!'");
+        TestRunner = new NGTestRunner(NGAllTestUnits.class);
+        TestRunner.InitTests();
+        TestRunner.RunTests();
     }
 
 }
